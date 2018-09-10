d532fa72dbaf8069ba3d4ebec1e16c6f97ff3f64
Sudhanshu Siddh <ssiddh@iu.edu>
2018-09-09 22:58:39 -0400

    Added documentation for LoginBloc

--- lib/blocs/login_bloc.dart
+++ lib/blocs/login_bloc.dart
@@ -4,12 +4,20 @@ import 'package:rxdart/rxdart.dart';
 class LoginBloc {
   final FirebaseAuth _firebaseAuth;
 
+  /// Stream that controls the login state
+  /// True if logged in, false otherwise and has error if there was an error
   final _isLoggedIn = PublishSubject<bool>();
+
+  /// Stream that stores the value of a FirebaseUser
   final _firebaseUser = BehaviorSubject<FirebaseUser>(seedValue: null);
 
   Observable<bool> get isLoggedIn => _isLoggedIn.stream;
   Observable<FirebaseUser> get getUser => _firebaseUser.stream;
 
+  /// Check if there is a user available
+  /// if not then [signInAnonymously] and get a user
+  /// change login state stream and user stream
+  /// on error add error to login state stream
   LoginBloc({FirebaseAuth firebaseAuth})
       : _firebaseAuth = firebaseAuth ?? FirebaseAuth.instance {
     _firebaseAuth.currentUser().then((FirebaseUser user) {
@@ -27,6 +35,7 @@ class LoginBloc {
     }).catchError((error) => _isLoggedIn.sink.addError("Failed to login"));
   }
 
+  /// Close all the streams
   void dispose() {
     _isLoggedIn.close();
     _firebaseUser.close();
