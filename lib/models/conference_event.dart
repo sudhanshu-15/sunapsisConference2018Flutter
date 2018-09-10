578898e9968bc30e7e62f63804c0cdbcc47b22e1
Sudhanshu Siddh <ssiddh@iu.edu>
2018-09-09 22:46:42 -0400

    Added tests for ConferenceEvent data object

--- lib/models/conference_event.dart
+++ lib/models/conference_event.dart
@@ -1,6 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 
+/// ConferenceEvent data object that represents a conference event.
+/// Has a named constructor which accepts a Map<String, dynamic> and created a ConferenceEvent object
 class ConferenceEvent {
+  /// Unique event id, identifier of Document on CloudFirestore on Firebase
   int _eventId;
   String _title;
   String _description;
@@ -9,9 +12,19 @@ class ConferenceEvent {
   String _endTime;
   String _audience;
   String _level;
+
+  /// List all possible resources, documents for a talk, ppt links etc
   List<String> _resources;
+
+  /// List of all the users who have marked this event as favorite
+  /// Is used to filter favorite events
   List<String> _favorite;
+
+  /// A reference to the presenter for a event.
+  /// Data is stored in the presenter document and is fetched later while creating the UI
   List<DocumentReference> _presenters;
+
+  //TODO: Add resource for location
 
   int get eventId => _eventId;
   String get title => _title;
