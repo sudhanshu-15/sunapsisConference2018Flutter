f3f963acb8bfde1b1d0353284f1206dc571e4131
Sudhanshu Siddh <ssiddh@iu.edu>
2018-09-19 01:07:37 -0400

    Created speaker detail page with hero animation

--- lib/widgets/speaker_card.dart
+++ lib/widgets/speaker_card.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:sunapsis_conference18/models/conference_speaker.dart';
+import 'package:sunapsis_conference18/pages/speaker_detail.dart';
 import 'package:sunapsis_conference18/utils/color_config.dart';
 import 'package:sunapsis_conference18/widgets/avatar_fade_image.dart';
 
@@ -10,37 +11,50 @@ class SpeakerCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      color: iuGreyLight,
-      child: Container(
-        child: GridTile(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisSize: MainAxisSize.min,
-            children: <Widget>[
-              GridTileBar(
-                  title: Text(
-                conferenceSpeaker.name,
-                style: Theme.of(context).textTheme.title,
-              )),
-              OrientationBuilder(
-                  builder: (BuildContext context, Orientation orientation) {
-                return SizedBox(
-                  width: orientation == Orientation.portrait ? 100.0 : 150.0,
-                  height: orientation == Orientation.portrait ? 100.0 : 150.0,
-                  child: AvatarFadeImage(conferenceSpeaker.picture),
-                );
-              }),
-              GridTileBar(
-                title: Chip(
-                    backgroundColor: iuGreyDark,
-                    label: Text(conferenceSpeaker.organization)),
-              ),
-            ],
+    return GestureDetector(
+      child: Card(
+        color: iuGreyLight,
+        child: Container(
+          child: GridTile(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                GridTileBar(
+                    title: Text(
+                  conferenceSpeaker.name,
+                  style: Theme.of(context).textTheme.title,
+                )),
+                OrientationBuilder(
+                    builder: (BuildContext context, Orientation orientation) {
+                  return SizedBox(
+                    width: orientation == Orientation.portrait ? 100.0 : 150.0,
+                    height: orientation == Orientation.portrait ? 100.0 : 150.0,
+                    child: Hero(
+                        tag: conferenceSpeaker,
+                        child: AvatarFadeImage(conferenceSpeaker.picture)),
+                  );
+                }),
+                GridTileBar(
+                  title: Chip(
+                      backgroundColor: iuGreyDark,
+                      label: Text(conferenceSpeaker.organization)),
+                ),
+              ],
+            ),
           ),
         ),
       ),
+      onTap: () {
+        Navigator.of(context).push(MaterialPageRoute(
+          builder: (BuildContext context) {
+            return SpeakerDetail(
+              speaker: conferenceSpeaker,
+            );
+          },
+        ));
+      },
     );
   }
 }
